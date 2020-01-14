7ac896d32823e9441c32277dcde76e2f9d4b6b4a
Sean Gonzalez <sean.oryn@gmail.com>
2020-01-13 20:10:39 -0600

    Made Generator functions for javascript.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// DOM elements
 var button = document.querySelector('button#generate');
 var result = document.querySelector('#result');
 var length = document.querySelector('#length');
@@ -7,19 +8,28 @@ var lowercase = document.querySelector('#lowercase');
 var uppercase = document.querySelector('#uppercase');
 var clipboard = document.querySelector('#clipboard')
 
-function generatePassword() {
-    return "blah";
+var randomFunc = {
+    lowercase: getRandomLower,
+    uppercase: getRandomUpper,
+    numeric: getRandomNumeric,
+    special: getRandomSpecial
+};
+
+// Generator Functions
+
+function getRandomLower() {
+    return String.fromCharCode(Math.floor(Math.random() * 26) + 97 );
 }
 
-
-button.addEventListener('click', event => {
-    password.value = generatePassword();
-  console.log(event);
-});
-
-const genPassButtonClick = event => {
-    password.value = generatePassword();
-    console.log(event);
+function getRandomUpper() {
+    return String.fromCharCode(math.floor(math.random() * 26) + 65);
 }
 
-button.addEventListener('click', genPassButtonClick);
+function getRandomNumeric() {
+    return String.fromCharCode(math.floor(math.random() * 10) + 48);
+}
+
+function getRandomSpecial() {
+    var symbols = '!@#$%^&*';
+    return symbols[math.floor(math.random() * symbols.length)];
+}
