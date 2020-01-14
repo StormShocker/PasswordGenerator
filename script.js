5b1b184e08f5e04d79688a4bc84b2f37bbd8dd66
Sean Gonzalez <sean.oryn@gmail.com>
2020-01-13 19:54:08 -0600

    Made DOM variables for javascript.

--- script.js
+++ script.js
@@ -1,21 +1,25 @@
-const button = document.querySelector('button#generate');
-var password = document.querySelector('#password');
+var button = document.querySelector('button#generate');
+var result = document.querySelector('#result');
+var length = document.querySelector('#length');
 var special = document.querySelector('#special');
 var numeric = document.querySelector('#numeric');
 var lowercase = document.querySelector('#lowercase');
 var uppercase = document.querySelector('#uppercase');
-
-// button.addEventListener('click', event => {
-//     password.value = generatePassword();
-//   console.log(event);
-// });
-button.addEventListener('click', genPassButtonClick);
+var clipboard = document.querySelector('#clipboard')
 
 function generatePassword() {
     return "blah";
 }
 
-function genPassButtonClick(event) {
+
+button.addEventListener('click', event => {
+    password.value = generatePassword();
+  console.log(event);
+});
+
+const genPassButtonClick = event => {
     password.value = generatePassword();
     console.log(event);
 }
+
+button.addEventListener('click', genPassButtonClick);
